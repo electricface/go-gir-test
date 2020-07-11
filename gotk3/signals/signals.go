c17d2222070386bacc423174d56e64d41af8e3ba
electricface <songwentai@uniontech.com>
2020-07-11 23:50:51 +0800

    add signals and simple

---
+++ gotk3/signals/signals.go
@@ -0,0 +1,46 @@
+package main
+
+import "github.com/linuxdeepin/go-gir/gtk-3.0"
+
+func main() {
+	gtk.Init(0, 0)
+	win := gtk.NewWindow(gtk.WindowTypeToplevel)
+	win.Connect(gtk.SigDestroy, gtk.MainQuit)
+	win.Add(windowWidget())
+	win.ShowAll()
+	gtk.Main()
+}
+
+func windowWidget() gtk.Widget {
+	grid := gtk.NewGrid()
+	grid.SetOrientation(gtk.OrientationVertical)
+
+	entry := gtk.NewEntry()
+	label := gtk.NewLabel("hello world label")
+	grid.Add(entry)
+	entry.SetHexpand(true)
+	grid.AttachNextTo(label, entry, gtk.PositionTypeRight, 1, 1)
+	label.SetHexpand(true)
+
+	entry.Connect(gtk.SigActivate, func() {
+		s := entry.GetText()
+		label.SetText(s)
+	})
+
+	sb := gtk.NewSpinButtonWithRange(0, 1, 0.1)
+	pb := gtk.NewProgressBar()
+	grid.Add(sb)
+	sb.SetHexpand(true)
+	grid.AttachNextTo(pb, sb, gtk.PositionTypeRight, 1, 1)
+	pb.SetHexpand(true)
+	sb.Connect(gtk.SigValueChanged, func() {
+		pb.SetFraction(sb.GetValue())
+	})
+
+	label = gtk.NewLabel("")
+	s := "Hyperlink to <a href=\"https://www.baidu.com/\">Baidu</a> for your clicking pleasure"
+	label.SetMarkup(s)
+	grid.AttachNextTo(label, sb, gtk.PositionTypeBottom, 2, 1)
+
+	return grid.Widget
+}
---
+++ gotk3/simple/simple.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/linuxdeepin/go-gir/gtk-3.0"
+	"log"
+)
+
+func main() {
+	gtk.Init(0, 0)
+	log.Println("hello")
+
+	win := gtk.NewWindow(gtk.WindowTypeToplevel)
+	win.SetTitle("simple example")
+	win.Connect(gtk.SigDestroy, gtk.MainQuit)
+
+	l := gtk.NewLabel("Hello, gotk3")
+	win.Add(l)
+	win.SetDefaultSize(800, 800)
+	win.ShowAll()
+
+	gtk.Main()
+}
