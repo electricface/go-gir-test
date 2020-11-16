a11d8b945dad218392f274cc0fcdbd23c61a9c20
electricface <songwentai@uniontech.com>
2020-11-16 21:52:49 +0800

    +1

--- callback/store/store_t1.go
+++ callback/store/store_t1.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"reflect"
-	"unsafe"
 
 	"github.com/linuxdeepin/go-gir/gdk-3.0"
 	"github.com/linuxdeepin/go-gir/gi"
@@ -25,7 +23,10 @@ func main() {
 		spew.Dump(args)
 		var arg0 gtk.Window
 		var ev gdk.Event
-		gi.Store(args, &arg0, &ev)
+		err := gi.Store(args, &arg0, &ev)
+		if err != nil {
+			log.Fatal(err)
+		}
 		log.Printf("arg0: %#v, arg1: %#v\n", arg0, ev)
 		_, btn := ev.GetButton()
 		_, x, y := ev.GetCoords()
@@ -36,9 +37,10 @@ func main() {
 			Window gtk.Window
 			Ev     gdk.Event
 		}
-		gi.StoreStruct(args, &args0)
-		//spew.Dump(args0)
-		//args0.Window.SetSizeRequest(300, 300)
+		err = gi.StoreStruct(args, &args0)
+		if err != nil {
+			log.Fatal(err)
+		}
 		ev = args0.Ev
 		_, btn = ev.GetButton()
 		_, x, y = ev.GetCoords()
@@ -48,74 +50,4 @@ func main() {
 		return true
 	})
 	gtk.Main()
-}
-
-func StoreStruct(args []interface{}, dest interface{}) {
-	rv := reflect.ValueOf(dest)
-	if rv.Kind() == reflect.Ptr {
-		log.Println("is ptr")
-		elem := rv.Elem()
-		if elem.Kind() == reflect.Struct {
-			num := elem.NumField()
-			for i := 0; i < num; i++ {
-				if i >= len(args) {
-					break
-				}
-				src := args[i]
-				f := elem.Field(i)
-				store(src, f.Addr().Interface())
-			}
-		}
-	}
-}
-
-func Store(args []interface{}, destSlice ...interface{}) {
-	for i, arg := range args {
-		if i >= len(destSlice) {
-			break
-		}
-		dest := destSlice[i]
-		store(arg, dest)
-	}
-}
-
-func store(src interface{}, dest interface{}) {
-	switch a := src.(type) {
-	//case g.Object:
-	//	left, ok := dest.(*g.Object)
-	//	if ok {
-	//		left.P = a.P
-	//	} else {
-	//		storeStructFieldP(dest, a.P)
-	//	}
-	case unsafe.Pointer:
-		left, ok := dest.(*unsafe.Pointer)
-		if ok {
-			*left = a
-		} else {
-			storeStructFieldP(dest, a)
-		}
-	default:
-		srcRv := reflect.ValueOf(src)
-		if srcRv.Kind() == reflect.Struct {
-			p := srcRv.FieldByName("P")
-			if p.Kind() == reflect.UnsafePointer {
-				storeStructFieldP(dest, unsafe.Pointer(p.Pointer()))
-			}
-		}
-
-	}
-}
-
-func storeStructFieldP(dest interface{}, ptr unsafe.Pointer) {
-	rv := reflect.ValueOf(dest)
-	if rv.Kind() == reflect.Ptr {
-		elem := rv.Elem()
-		if elem.Kind() == reflect.Struct {
-			p := elem.FieldByName("P")
-			if p.IsValid() && p.Kind() == reflect.UnsafePointer {
-				p.SetPointer(ptr)
-			}
-		}
-	}
 }
